e4e8b958f3e83abe2f047229f50a97fcdfc8afdb
Florian Tönjes <skatepainter@yahoo.de>
2020-08-10 15:14:13 +0200

    Added Identity classes to injector

--- ComicBookLibraryManagerWebApp/App_Start/SimpleInjectorInitializer.cs
+++ ComicBookLibraryManagerWebApp/App_Start/SimpleInjectorInitializer.cs
@@ -3,8 +3,14 @@
 namespace ComicBookLibraryManagerWebApp.App_Start
 {
     using System.Reflection;
+    using System.Web;
     using System.Web.Mvc;
     using ComicBookShared.Data;
+    using ComicBookShared.Models;
+    using ComicBookShared.Security;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.Owin;
     using SimpleInjector;
     using SimpleInjector.Integration.Web;
     using SimpleInjector.Integration.Web.Mvc;
@@ -37,6 +43,19 @@ namespace ComicBookLibraryManagerWebApp.App_Start
 
             container.Register<ComicBookArtistsRepository>(Lifestyle.Scoped);
             container.Register<SeriesRepository>(Lifestyle.Scoped);
+
+            container.Register<ApplicationUserManager>(Lifestyle.Scoped);
+            container.Register<ApplicationSignInManager>(Lifestyle.Scoped);
+
+            container.Register(() =>
+            container.IsVerifying
+                ? new OwinContext().Authentication
+                : HttpContext.Current.GetOwinContext().Authentication,
+            Lifestyle.Scoped);
+
+            container.Register<IUserStore<User>>(() =>
+                        new UserStore<User>(container.GetInstance<Context>()),
+                        Lifestyle.Scoped);
         }
     }
 }
--- ComicBookLibraryManagerWebApp/Controllers/AccountController.cs
+++ ComicBookLibraryManagerWebApp/Controllers/AccountController.cs
@@ -1,16 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookLibraryManagerWebApp.ViewModels;
+using ComicBookShared.Security;
+using Microsoft.Owin.Security;
 
 namespace ComicBookLibraryManagerWebApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationUserManager _userManager;
+        private readonly ApplicationSignInManager _signInManager;
+        private readonly IAuthenticationManager _authenticationManager;
+
+        public AccountController(
+            ApplicationUserManager userManager,
+            ApplicationSignInManager signInManager,
+            IAuthenticationManager authenticationManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _authenticationManager = authenticationManager;
+        }
+
         public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(AccountRegisterViewModel viewModel)
+        {
+            var existingUser = await _userManager.FindByEmailAsync(viewModel.Email);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("Email", $"The provided email address '{viewModel.Email}' has already been used to register an account. Please sign-in using your existing account.");
+            }
+
+            return View(viewModel);
         }
     }
 }
