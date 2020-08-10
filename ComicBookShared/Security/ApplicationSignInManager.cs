fe472dc787b436b323604138ef5a5c433682e691
Florian Tönjes <skatepainter@yahoo.de>
2020-08-10 14:46:35 +0200

    Started implementing user management. Installed dependencies.

---
+++ ComicBookShared/Security/ApplicationSignInManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBookShared.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace ComicBookShared.Security
+{
+    public class ApplicationSignInManager : SignInManager<User, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager) : base(userManager, authenticationManager)
+        { }
+    }
+}
---
+++ ComicBookShared/Security/ApplicationUserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBookShared.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ComicBookShared.Security
+{
+    public class ApplicationUserManager : UserManager<User>
+    {
+        public ApplicationUserManager(IUserStore<User> userStore) : base(userStore)
+        { }
+    }
+}
